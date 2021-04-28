840bbc0c296071565e4b1b3a6f49f29497a89cbd
Dahbia BERRANI <dehbia.berrani1993@gmail.com>
2021-04-28 17:25:28 +0200

    function mise_ajour

--- tamagotchi/js/scriptTamagotchi.js
+++ tamagotchi/js/scriptTamagotchi.js
@@ -1,17 +1,4 @@
-var intervale = setInterval("enVie()",2000);
-
-function enVie() {
-    faim.textContent = dansInterval(parseInt(faim.textContent) - 1);
-    
-    soif.textContent = dansInterval(parseInt(soif.textContent) - 1);
-    sante.textContent =dansInterval( parseInt(sante.textContent) - 1);
-
-    if ( (parseInt(faim.textContent) <= 0) || (parseInt(soif.textContent) <= 0) || (parseInt(sante.textContent) <= 0) ){		
-        clearInterval(intervale); 
-        //cacher bouton ou empecher utilsation
-    }
-}
-
+var intervale = setInterval("vivre()",2000);
 // fonction qui garantie que la valeur des cractéristiques restent entre 0 et 100  
 function dansInterval(valeur){
     if (valeur > 100) {
@@ -26,12 +13,34 @@ function dansInterval(valeur){
 
     return returnvaleur;
 }
+function setFaim(value){
+    valeurDansLesBornes = dansInterval(value);
+    //Mise à jour de l'interface graphique
+    faim.textContent = valeurDansLesBornes;
+    var faimProgressBarValue = document.getElementById("faimBar");              
+    faimProgressBarValue.style.width = valeurDansLesBornes + "%"; 
+}
+
+function vivre() {
+    setFaim( parseInt(faim.textContent) - 1 );
+    soif.textContent = dansInterval(parseInt(soif.textContent) - 1);
+    sante.textContent =dansInterval( parseInt(sante.textContent) - 1);
+    dormir.textContent =dansInterval( parseInt(dormir.textContent) - 1);
+    jouer.textContent =dansInterval( parseInt(jouer.textContent) - 1);
+
+    if ( (parseInt(faim.textContent) <= 0) || (parseInt(soif.textContent) <= 0) || (parseInt(sante.textContent) <= 0) || (parseInt(jouer.textContent) <= 0) || (parseInt(dormir.textContent) <= 0)){		
+        clearInterval(intervale); 
+        //cacher bouton ou empecher utilsation
+    }
+}
+
+
 
 document.addEventListener('DOMContentLoaded', init, false);
 
 function init(){
     function nourrir(){
-        faim.textContent = dansInterval( parseInt(faim.textContent) + 5 ); 
+        setFaim( parseInt(faim.textContent) + 5 );
     }
     document.getElementById('Nourrir').addEventListener('click', nourrir, true);
 
@@ -44,5 +53,10 @@ function init(){
         sante.textContent =  dansInterval(parseInt(sante.textContent) + 5) ; 
     }
     document.getElementById('Soigner').addEventListener('click',soigner, true);
+
+
+  
+
 };
 
+// progress bar 
