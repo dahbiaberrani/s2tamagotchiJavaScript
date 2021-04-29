98e8a8ceec87e403dca429d332fbb012bc7935de
Dahbia BERRANI <dehbia.berrani1993@gmail.com>
2021-04-29 14:18:00 +0200

    progressBars marche

--- tamagotchi/js/scriptTamagotchi.js
+++ tamagotchi/js/scriptTamagotchi.js
@@ -13,23 +13,69 @@ function dansInterval(valeur){
 
     return returnvaleur;
 }
+
+function setProgressBarValue(value,progressBarName){ 
+     
+    var elmentProgressBarValue = document.getElementById(progressBarName);  
+    elmentProgressBarValue.style.width =  value + "%";
+    // mise à jours de la couleur 
+    if (value >50) {
+        color =  "green";
+    }else if  (value > 25){
+        color  =  "orange";
+    }else{
+        color  = "red";
+    }
+    elmentProgressBarValue.style.background = color;
+}
+
+    
+
+
 function setFaim(value){
     valeurDansLesBornes = dansInterval(value);
     //Mise à jour de l'interface graphique
     faim.textContent = valeurDansLesBornes;
-    var faimProgressBarValue = document.getElementById("faimBar");              
-    faimProgressBarValue.style.width = valeurDansLesBornes + "%"; 
+    setProgressBarValue(valeurDansLesBornes,"faimBar");
+}
+function setSoif(value){
+    valeurDansLesBornes = dansInterval(value);
+    //Mise à jour de l'interface graphique
+    soif.textContent = valeurDansLesBornes;
+    setProgressBarValue(valeurDansLesBornes,"soifBar")
+}
+
+function setSante(value){
+    valeurDansLesBornes = dansInterval(value);
+    //Mise à jour de l'interface graphique
+    sante.textContent = valeurDansLesBornes;
+    setProgressBarValue(valeurDansLesBornes,"santeBar")
+}
+
+function setDormir(value){
+    valeurDansLesBornes = dansInterval(value);
+    //Mise à jour de l'interface graphique
+    dormir.textContent = valeurDansLesBornes;
+    setProgressBarValue(valeurDansLesBornes,"dormirBar")
+}
+function setHummeur(value){
+    valeurDansLesBornes = dansInterval(value);
+    //Mise à jour de l'interface graphique
+    hummeur.textContent = valeurDansLesBornes;
+    setProgressBarValue(valeurDansLesBornes,"hummeurBar")
 }
 
 function vivre() {
     setFaim( parseInt(faim.textContent) - 1 );
-    soif.textContent = dansInterval(parseInt(soif.textContent) - 1);
-    sante.textContent =dansInterval( parseInt(sante.textContent) - 1);
-    dormir.textContent =dansInterval( parseInt(dormir.textContent) - 1);
-    jouer.textContent =dansInterval( parseInt(jouer.textContent) - 1);
+    setSoif( parseInt(soif.textContent) - 1 );
+    
+    setSante( parseInt(sante.textContent) - 1);
+    setDormir( parseInt(dormir.textContent) - 1);
+    setHummeur( parseInt(hummeur.textContent) - 1);
 
+    // mort de tamagotchi
     if ( (parseInt(faim.textContent) <= 0) || (parseInt(soif.textContent) <= 0) || (parseInt(sante.textContent) <= 0) || (parseInt(jouer.textContent) <= 0) || (parseInt(dormir.textContent) <= 0)){		
-        clearInterval(intervale); 
+       clearInterval(intervale); 
         //cacher bouton ou empecher utilsation
     }
 }
@@ -41,22 +87,29 @@ document.addEventListener('DOMContentLoaded', init, false);
 function init(){
     function nourrir(){
         setFaim( parseInt(faim.textContent) + 5 );
+        
     }
     document.getElementById('Nourrir').addEventListener('click', nourrir, true);
 
     function boire(){
-        soif.textContent = dansInterval( parseInt(soif.textContent) + 5 );
+        setSoif( parseInt(soif.textContent) + 5 );
     }
     document.getElementById('Boire').addEventListener('click',boire, true);
 
     function soigner(){
-        sante.textContent =  dansInterval(parseInt(sante.textContent) + 5) ; 
+        setSante(parseInt(sante.textContent) + 5 );
     }
     document.getElementById('Soigner').addEventListener('click',soigner, true);
 
+    function dormir(){
+        setDormir(parseInt(dormir.textContent) + 5 );
+    }
+    document.getElementById('Dormir').addEventListener('click',dormir, true);
 
-  
-
+    function hummeur(){
+        setHummeur(parseInt(hummeur.textContent) + 5 );
+    }
+    document.getElementById('Jouer').addEventListener('click',hummeur, true);
 };
 
 // progress bar 
